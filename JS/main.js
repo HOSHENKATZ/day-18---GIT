162db2f7ef2bff05266f6a65c6af0b5d2fdfd355
hoshen katz <hoshen.katz@gmail.com>
2024-12-24 15:53:56 +0200

    Sizing amount changes

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,16 @@
 var gBallSize = 100
 function onBallClick() {
     var ball = document.querySelector('.ball')
-    var ballSize = ball.innerText
+    var ballSizeToAdd = getRandomIntInclusive(20, 60)
     console.dir(ball)
-    ball.style.width = gBallSize + 50
-    ball.style.height = gBallSize + 50
-    ball.innerText = gBallSize + 50
-    gBallSize += 50
+    ball.style.width = gBallSize + ballSizeToAdd
+    ball.style.height = gBallSize + ballSizeToAdd
+    ball.innerText = gBallSize + ballSizeToAdd
+    gBallSize += ballSizeToAdd
 
-    if (gBallSize> 400){
+    if (gBallSize > 400) {
         gBallSize = 100
-        ball.style.width = gBallSize 
+        ball.style.width = gBallSize
         ball.style.height = gBallSize
         ball.innerText = gBallSize
     }
--- JS/util.js
+++ JS/util.js
@@ -2,3 +2,4 @@ function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
