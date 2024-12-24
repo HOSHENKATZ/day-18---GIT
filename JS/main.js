fff34e4e5683a9e8378a78ca8a132988fe7a73c5
hoshen katz <hoshen.katz@gmail.com>
2024-12-24 16:03:19 +0200

    rand color to ball

--- JS/main.js
+++ JS/main.js
@@ -3,16 +3,20 @@ var gBallSize = 100
 function onBallClick() {
     var ball = document.querySelector('.ball')
     var ballSizeToAdd = getRandomIntInclusive(20, 60)
-    console.dir(ball)
+   console.dir(ball)
     ball.style.width = gBallSize + ballSizeToAdd
     ball.style.height = gBallSize + ballSizeToAdd
     ball.innerText = gBallSize + ballSizeToAdd
     gBallSize += ballSizeToAdd
+    ball.style.backgroundColor = getRandomColor()
 
     if (gBallSize > 400) {
         gBallSize = 100
         ball.style.width = gBallSize
         ball.style.height = gBallSize
         ball.innerText = gBallSize
+        ball.style.backgroundColor = 'dodgerblue'
     }
+
+   
 }
