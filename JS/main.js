0c498a8c0c13698ac3e83f1f966aedb3b83d2ee9
hoshen katz <hoshen.katz@gmail.com>
2024-12-24 15:12:59 +0200

    Func and style changes

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,11 @@
 'use strict'
-function onBallClick(){
-    console.log('the ball clicked!!!!')
+var gBallSize = 100
+function onBallClick() {
+    var ball = document.querySelector('.ball')
+    var ballSize = ball.innerText
+    console.dir(ball)
+    ball.style.width = gBallSize + 50
+    ball.style.height = gBallSize + 50
+    ball.innerText = gBallSize + 50
+    gBallSize += 50
 }
